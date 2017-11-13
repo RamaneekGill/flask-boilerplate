bf92dc1d3c0b264fedeb37d64baf5040c808167f
Ramaneek Gill <gill.ramaneek@gmail.com>
2017-11-13 14:16:59 -0500

    Add data marshalling - return only set attributes from model

--- app/app.py
+++ app/app.py
@@ -5,15 +5,20 @@ App boilerplate
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import reqparse, abort, Api, Resource, fields, marshal_with
 from logzero import logger
 
 DEFAULT_PORT = 8000
 
-TASKS = {  # Collection of Models
-    'task1': {'task': 'build an API'},
-    'task2': {'task': '?????'},
-    'task3': {'task': 'profit!'},
+TASKS = [  # Collection of models
+    {'id': 1, 'task': 'build an API', 'other': 'hi'},
+    {'id': 2, 'task': '?????'},
+    {'id': 3, 'task': 'profit!'}
+]
+
+TASK_FIELDS = {
+    'task': fields.String,
+    'id': fields.Integer
 }
 
 
@@ -28,7 +33,7 @@ def abort_if_task_doesnt_exist(task_id):
         ### Raises
            - __HTTPException__: 404 when Task ID not found
     """
-    if task_id not in TASKS:
+    if any(task['id'] == task_id for task in TASKS):
         abort(404, message="Task {} doesn't exist".format(task_id))
 
 
@@ -48,23 +53,37 @@ def create_app(config=None):
                                        help='No `task` provided', location='json')
             super(TasksAPI, self).__init__()
 
+        def _get_task(self, task_id):
+            """Find a task from the collection based on ID else return None"""
+            for task in TASKS:
+                if str(task['id']) == str(task_id):
+                    return task
+            return None
+
+        @marshal_with(TASK_FIELDS)
         def get(self, task_id):
             """GET a task"""
             abort_if_task_doesnt_exist(task_id)
-            return TASKS[task_id]
+            return self._get_task(task_id)
 
+        @marshal_with(TASK_FIELDS)
         def delete(self, task_id):
             """Delete a task"""
             abort_if_task_doesnt_exist(task_id)
-            del TASKS[task_id]
+            task = self._get_task(task_id)
+            TASKS.remove(task)
             return '', 204
 
+        @marshal_with(TASK_FIELDS)
         def put(self, task_id):
             """Update a task"""
             args = self.reqparse.parse_args()
-            task = {'task': args['task']}
-            TASKS[task_id] = task
-            return task, 201
+            for i in range(len(TASKS)):
+                if TASKS[i]['id'] == task_id:
+                    TASKS[i]['task'] = args['task']
+                    return TASKS[i], 201
+
+            abort_if_task_doesnt_exist(task_id)
 
     class TasksListAPI(Resource):
         """APIs for a list of Tasks"""
@@ -74,20 +93,21 @@ def create_app(config=None):
                                        help='No `task` provided', location='json')
             super(TasksListAPI, self).__init__()
 
+        @marshal_with(TASK_FIELDS)
         def get(self):
             """Get a list of all Tasks"""
             return TASKS
 
+        @marshal_with(TASK_FIELDS)
         def post(self):
             """Create a new Task"""
             args = self.reqparse.parse_args()
-            task_id = int(max(TASKS.keys()).lstrip('task')) + 1
-            task_id = 'task{}'.format(task_id)
-            TASKS[task_id] = {'task': args['task']}
+            task_id = len(TASKS)
+            TASKS.append({'id': task_id, 'task': args['task']})
             return TASKS[task_id], 201
 
     api.add_resource(TasksListAPI, '/tasks')
-    api.add_resource(TasksAPI, '/tasks/<todo_id>')
+    api.add_resource(TasksAPI, '/tasks/<task_id>')
 
     @app.route("/")
     def hello_world():
